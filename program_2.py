3c89e0f0dfc7698efda00871e34888d0eb756fcc
jtphillips09 <jtphillips@students.unwsp.edu>
2025-10-24 14:07:23 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,16 +1,15 @@
-# Program #2: Word Separator
-# Write a program that accepts as input a sentence in which all of the words are run together, 
-# but the first character of each word is uppercase.  
-# Convert the sentence to a string in which the words are separated by spaces, 
-# and the first word starts with an uppercase.  
-# For example the string "StopAndSmellTheRoses" would be converted to "Stop and smell the roses."
-
-# Start your changes on line 13
-
 def word_separator(sentence):
 
     new_sentence = ""
     #    Add your logic here
+    for i, char in enumerate(sentence):
+        if char.isupper() and i != 0:
+            new_sentence += " " + char.lower()
+        else:
+            new_sentence += char
+
+    # Make sure the first letter of the sentence is capitalized
+    new_sentence = new_sentence[0].upper() + new_sentence[1:]
 
     return new_sentence.strip()
 
